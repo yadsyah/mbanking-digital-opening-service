16c43d60e24d773619db50860c71f57c4324834b
yadsyah <diyansetiyadi@gmail.com>
2022-12-31 18:43:46 +0700

    feat: add log appender stream kafka

--- src/main/java/id/co/diansetiyadi/digitalopeningservice/config/KafkaProducerConfig.java
+++ src/main/java/id/co/diansetiyadi/digitalopeningservice/config/KafkaProducerConfig.java
@@ -1,10 +1,12 @@
 package id.co.diansetiyadi.digitalopeningservice.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -35,5 +37,10 @@ public class KafkaProducerConfig {
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public NewTopic logNotificationTopic() {
+        return TopicBuilder.name("digital-opening-log").partitions(10).replicas(1).build();
     }
 }
--- src/test/java/id/co/diansetiyadi/digitalopeningservice/DigitalOpeningServiceApplicationTests.java
+++ src/test/java/id/co/diansetiyadi/digitalopeningservice/DigitalOpeningServiceApplicationTests.java
@@ -1,13 +1,43 @@
 package id.co.diansetiyadi.digitalopeningservice;
 
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.library.Architectures;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DigitalOpeningServiceApplicationTests {
 
+	private static JavaClasses importedClasses;
+
+
+	@BeforeAll
+	static void setUp() {
+		ImportOption ignoreTests = location -> {
+			return !location.contains("/test/"); // ignore any URI to sources that contains '/test/'
+		};
+		importedClasses = new ClassFileImporter().withImportOption(ignoreTests).importPackages("id.co.diansetiyadi");
+	}
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void architectPatternGuardianTest() {
+		Architectures.LayeredArchitecture layeredArchitecture = Architectures.layeredArchitecture()
+				.layer("Controller").definedBy("..controller..")
+				.layer("Service").definedBy("..service..")
+				.layer("Repository").definedBy("..repository..")
+				.whereLayer("Controller").mayNotBeAccessedByAnyLayer()
+				.whereLayer("Service").mayOnlyBeAccessedByLayers("Controller", "Service")
+				.whereLayer("Repository").mayOnlyBeAccessedByLayers("Service");
+
+		layeredArchitecture.check(importedClasses);
+
 	}
 
 }
